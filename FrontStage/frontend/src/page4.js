6736da2a0c9df7d93c1e52df28984ae20f0ee666
moto12508 <a9527433@gmail.com>
2023-09-27 16:12:20 +0800

    修改禮物盒rwd

--- FrontStage/frontend/src/page4.js
+++ FrontStage/frontend/src/page4.js
@@ -4,7 +4,10 @@ import cakepackage from './images/專題2/包裝圖片.png'; // 导入图片
 import step1 from './images/專題2/step1.png';
 import step2 from './images/專題2/step2.png';
 import step3 from './images/專題2/step3.png';
-import arrow from './images/專題2/箭頭.png';
+import number1 from './images/專題2/數字1.png';
+import number2 from './images/專題2/數字2.png';
+import number3 from './images/專題2/數字3.png';
+import number4 from './images/專題2/數字4.png';
 import step4 from './images/專題2/logo圖標.png';
 // import { Link } from 'react-router-dom';
 function page4() {
@@ -14,7 +17,7 @@ function page4() {
       <h1 className="card-title fw-bold">客製化禮物盒</h1>
 
       <div className="container-fluid ">
-        <div className="row m-5">
+        <div className="row m-3">
           <div className="col-md-5">
             <img
               className="card-img-top shadow p-3 img4"
@@ -33,10 +36,10 @@ function page4() {
                   style={{ width: '120px' }}
                 />
                 
-                <p>選擇禮物規格</p>
+                <p>選擇規格</p>
                 <img
-                  className="arrow"
-                  src={arrow}
+                  className=""
+                  src={number1}
                   alt="方向"
                   style={{ width: '70px' }}
                 />
@@ -52,13 +55,13 @@ function page4() {
 
                 <p>選擇商品</p>
                 <img
-                  className="arrow arrow2"
-                  src={arrow}
+                  className=""
+                  src={number2}
                   alt="方向"
                   style={{ width: '70px' }}
                 />
               </div>
-              <div className="col ">
+              <div className="col">
                 <img
                   className="m-2 img-fluid"
                   src={step3}
@@ -66,39 +69,35 @@ function page4() {
                   style={{ width: '120px' }}
                 />
                 <br />
-                <p>選擇貼心小卡</p>
+                <p>選擇小卡</p>
                 <img
-                  className="arrow"
-                  src={arrow}
+                  className=""
+                  src={number3}
                   alt="方向"
                   style={{ width: '70px' }}
                 />
               </div>
-              <div className="col ">
+              <div className=" ">
                 <img
                   className="m-2 img-fluid"
                   src={step4}
                   alt="客製化禮盒"
-                  style={{ width: '130px' }}
+                  style={{ width: '200px' }}
                 />
 
-                <h3>
-                  完成
-                  <br />
-                  <br />
-                  <br />
-                </h3>
-              </div>
-            </div>
-            
-          </div>
-          <div className="row">
+<div className="row">
           <div className="col-md-12">
-            <a href="/giftbox" className="btn btn-danger bbb">
+            <br/>
+            <a href="/giftbox" className="btn btn-danger ">
               開始製作禮物盒
             </a>
           </div>
         </div>
+              </div>
+            </div>
+            
+          </div>
+         
         </div>
         
       </div>
